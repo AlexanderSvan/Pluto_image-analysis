204e6001da13ffd886b57b739c0303db479fe4ac
AlexanderSvan <xalba.svan@gmail.com>
2021-04-20 12:52:44 +0200

    branch cleanup

--- data_plot.py
+++ data_plot.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 
 with open('C:/Users/xalba/Desktop/data2.json') as f:
   data = json.load(f)
   
-    df=pd.DataFrame.from_dict(data, orient='index')
-    
-    df.T.plot()
+df=pd.DataFrame.from_dict(data, orient='index')
+
+df.T.plot()
+plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
